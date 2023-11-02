c1f3fcffdd97404c10356abaa382beb9ebfdc04e
DESKTOP-O2UJ1K9\Joonatan <huang.joonatan@gmail.com>
2023-11-02 21:09:44 +0200

    task1.8: add tests to bring varasto coverage to 100%

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,40 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_konstruktori_nollaa_negatiivisen_tilavuuden(self):
+        virheellinen_varasto = Varasto(-1)
+
+        self.assertAlmostEqual(virheellinen_varasto.tilavuus, 0)
+
+    def test_konstruktori_nollaa_negatiivisen_alku_saldon(self):
+        virheellinen_varasto = Varasto(10, -2)
+
+        self.assertAlmostEqual(virheellinen_varasto.saldo, 0)
+
+    def test_negatiivinen_lisays_ei_muuta_saldoa(self):
+        self.varasto.lisaa_varastoon(-10)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_tilavuuden_yli_lisääminen_täyttää_varaston(self):
+        self.varasto.lisaa_varastoon(15)
+
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_negatiivinen_ottaminen_palauttaa_nolla(self):
+        saatu_maara = self.varasto.ota_varastosta(-5)
+
+        self.assertAlmostEqual(saatu_maara, 0)
+
+    def test_saldon_yli_ottaminen_palauttaa_jaljella_olevan_saldon(self):
+        self.varasto.lisaa_varastoon(7)
+
+        saatu_maara = self.varasto.ota_varastosta(9)
+
+        self.assertAlmostEqual(saatu_maara, 7)
+
+    def test_str_palauttaa_varaston_saldon_ja_vapaan_tilan_oikein(self):
+        printti = self.varasto.__str__()
+
+        self.assertEqual(printti, "saldo = 0, vielä tilaa 10")
